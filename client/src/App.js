93df52c83dceb84e333884f953aa3fb5f6e38285
Elena Smith <elenasmith@Elenas-MacBook-Air.local>
2021-01-03 11:49:46 -0600

    Removed csvreader component due to error in build

--- client/src/App.js
+++ client/src/App.js
@@ -140,11 +140,11 @@ export default class App extends PureComponent {
       <div>
         <Header />
          <div><button onClick={this.handleButton}>Add Column</button></div>
-          <CSVReader onFileLoaded={this.handleForce} />
       </div>
     );
   }
 }
+//<CSVReader onFileLoaded={this.handleForce} />
 // <FileResults data={this.state.data} columns={this.state.columns} updateData ={(event) => this.dataUpdater(event)}/>
 // <button><CSVLink data={this.state.data} headers={this.state.header}>Download me</CSVLink></button>
 // <BarChartByCategory data={this.state.data} />
